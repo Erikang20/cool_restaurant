c88af15659836d31e3f369e607dbc6b37ad3e77c
Erika Angarita <erikang20@gmail.com>
2024-09-05 16:16:13 -0600

    rendering buttons

--- app/main.js
+++ app/main.js
@@ -1,9 +1,10 @@
+import { createNewElement, formatPrice } from "./util.js";
+
 document.addEventListener("DOMContentLoaded", () => {
-	fetch("/desserts") // Fetch from the new /desserts endpoint
+	fetch("/desserts")
 		.then((response) => response.json())
 		.then((data) => {
 			renderData(data);
-			console.log(data);
 		})
 		.catch((error) => console.error("Uh-oh, something went wrong 🫤:", error));
 });
@@ -13,16 +14,34 @@ function renderData(data) {
 	const newList = data
 		.map((item, index) => {
 			return `
-			<div class="item-${index}" key="${index}">
-				<img src="${item.image.thumbnail}" alt="${item.name}" />
-				<div>Name: ${item.name}</div>
-				<div>Category: ${item.category}</div>
-				<div>Price: $${item.price}</div>
+			<div class="item" key="${index}">
+        <img class="image" src="${item.image.thumbnail}" alt="${item.name}" />
+        <div class="addToCart">
+          <link href="#">
+            <button>-</button>
+            Add to Cart
+            <button>+</button>
+          </div>
+        </link>
+        <div class="innerContainer">
+          <div class="category">${item.category}</div>
+          <div class="name">${item.name}</div>
+          <div class="price">${formatPrice(item.price)}</div>
+        </div>
 			</div>
 		`;
 		})
-		.join(""); // Join all HTML strings into one string
+		.join("");
 
-	// Set the innerHTML of the container
 	container.innerHTML = newList;
+	addEventListeners();
+}
+
+function addEventListeners() {
+	const buttons = document.querySelectorAll(".addToCart");
+	buttons.forEach((button, index) => {
+		button.addEventListener("click", () => {
+			console.log(`Added item ${index + 1} to cart`);
+		});
+	});
 }
--- app/util.js
+++ app/util.js
@@ -1,19 +1,19 @@
-export const createGameElement = (tag, className) => {
+export const createNewElement = (tag, className) => {
 	const element = document.createElement(tag);
 	element.className = className;
 	return element;
 };
 
-// const CURRENCY = new Intl.NumberFormat(undefined, {
-// 	currency: "USD",
-// 	style: "currency",
-// });
+const CURRENCY = new Intl.NumberFormat(undefined, {
+	currency: "USD",
+	style: "currency",
+});
 
-// /**
-//  *
-//  * @param price
-//  * @returns a formatted number as USD currency.
-//  */
-// const formatPrice = (price) => {
-// 	return CURRENCY.format(price);
-// };
+/**
+ *
+ * @param price
+ * @returns a formatted number as USD currency.
+ */
+export const formatPrice = (price) => {
+	return CURRENCY.format(price);
+};

a18a6fb127815b83e3cc00650b939c756a9795e7
Erika Angarita <erikang20@gmail.com>
2024-09-10 15:32:26 -0600

    Displaying elements in the Cart

--- app/main.js
+++ app/main.js
@@ -1,5 +1,7 @@
 import { createNewElement, formatPrice } from "./util.js";
-import { renderCart } from "./cart.js";
+import { renderCart, addToCart } from "./cart.js";
+
+let cart = [];
 
 document.addEventListener("DOMContentLoaded", () => {
 	fetch("/desserts")
@@ -17,15 +19,20 @@ function renderData(data) {
 			return `
 			<div class="item" key="${index}">
         <img class="image" src="${item.image.thumbnail}" alt="${item.name}" />
-        <div class="addToCart addLink">
-          <span class="add">
+        <div class="addToCart" data-id="${index}">
+          <div class="add">
             <span class="icon add"></span>
-            Add to Cart
-          </span>
-          <span class="hideBtns">
-            <button class="addButton hide">-</button>
-            <button class="addButton hide">+</button>
-          </span>
+            <span class="add">Add to Cart</span>
+          </div>
+          <div class="hideBtns">
+            <button class="addButton hide">
+              <img class="icon-btn" src="/images/icon-decrement-quantity.svg"/>
+            </button>
+            <span class="addButton hide">1</span>
+            <button class="addButton hide">
+              <img class="icon-btn" src="/images/icon-increment-quantity.svg"/>
+            </button>
+          </div>
         </div>
         <div class="innerContainer">
           <div class="category">${item.category}</div>
@@ -39,16 +46,26 @@ function renderData(data) {
 
 	container.innerHTML = newList;
 	renderCart();
-	addEventListeners();
+	addEventListeners(data);
 }
 
-function addEventListeners() {
+function addEventListeners(data) {
 	const buttons = document.querySelectorAll(".addToCart");
 
 	buttons.forEach((button) => {
 		button.addEventListener("click", () => {
-			const toRemove = button.querySelectorAll(".add");
-			toRemove.forEach((el) => {
+			const itemId = button.dataset.id;
+			const selectedItem = data[itemId];
+
+			if (selectedItem) {
+				addToCart(selectedItem);
+			}
+
+			const btnsToHide = button.querySelectorAll(".add");
+			const selectedParent = button.closest(".addToCart");
+			selectedParent.classList.add("multi-btns");
+
+			btnsToHide.forEach((el) => {
 				el.classList.add("hidden");
 				el.classList.remove("hide");
 			});
@@ -57,9 +74,34 @@ function addEventListeners() {
 			if (hideBtns) {
 				const hiddenElements = hideBtns.querySelectorAll(".addButton");
 				hiddenElements.forEach((el) => {
+					el.classList.add("counterBtns");
 					el.classList.remove("hide");
 				});
 			}
 		});
 	});
+
+	const itemSelected = document.querySelectorAll(".item");
+	itemSelected.forEach((item) => {
+		item.addEventListener("click", (e) => {
+			e.stopPropagation();
+			const firstChild = item.firstElementChild;
+			console.log(firstChild);
+			firstChild.classList.add("selectedImg");
+		});
+	});
 }
+
+// function updateCartUI(data) {
+// 	const cartContainer = document.querySelector(".cart-container");
+// 	cartContainer.innerHTML = "";
+
+// 	cart.forEach((item) => {
+// 		const itemElement = document.createElement("div");
+// 		itemElement.innerHTML = `
+//             <div>${item.name} - ${item.quantity} x $${item.price}</div>
+//             <button onclick="removeFromCart(${item.id})">Remove</button>
+//         `;
+// 		cartContainer.appendChild(itemElement);
+// 	});
+// }
